8418bbfc0564632e37fb7c676c01f4b7537ec984
gulerkerimm <gulerkerimm@gmail.com>
2024-12-11 23:41:45 +0300

    first commit

--- src/main/java/com/tpe/hb02/embeddable/RunnerFetch02.java
+++ src/main/java/com/tpe/hb02/embeddable/RunnerFetch02.java
@@ -7,13 +7,13 @@ import org.hibernate.cfg.Configuration;
 public class RunnerFetch02 {
     public static void main(String[] args) {
 
-        Configuration config=new Configuration().configure().addAnnotatedClass(com.tpe.hb02.embeddable.Student02.class);
+        Configuration config=new Configuration().configure().addAnnotatedClass(Student02.class);
         SessionFactory sf=config.buildSessionFactory();
         Session session =sf.openSession();
 
         //id:1002 olan ögrencinin tüm bilgilerini görelim
 
-         com.tpe.hb02.embeddable.Student02 student=session.get(com.tpe.hb02.embeddable.Student02.class,1002);
+         Student02 student=session.get(Student02.class,1002);
         System.out.println(student);
 
         //öğrencinin adresini görelim
---
+++ src/main/java/com/tpe/hb03/uni_onetoone/RunnerFetch03.java
@@ -0,0 +1,49 @@
+package com.tpe.hb03.uni_onetoone;
+
+import com.tpe.hb02.embeddable.Student02;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class RunnerFetch03 {
+    public static void main(String[] args) {
+
+
+        Configuration config = new Configuration().configure().addAnnotatedClass(Student03.class)//hibernate.cfg.xml
+                .addAnnotatedClass(Diary.class);
+        SessionFactory sessionFactory = config.buildSessionFactory();
+        Session session = sessionFactory.openSession();
+        //id 1001 olan öğrenciyi goruntuleyelim
+        Student03 student = session.get(Student03.class, 1001);
+        System.out.println(student);
+
+        //id 11 olan günlüğü getirelim
+        Diary diary=session.get(Diary.class,11);
+        System.out.println(diary);
+
+        System.out.println("*******************************");
+        System.out.println(student);
+        System.out.println("******************************");
+        System.out.println(diary);
+        System.out.println("******************************");
+
+        //günlük kime ait?
+        System.out.println(diary.getStudent());//günlüğe sahip olan öğrencinin bilgilerini getir
+
+        //id 1002 olan öğrenciye ait günlüğü bulmak istersek ne yapıcaz?
+        Student03 student2=session.get(Student03.class,1002);
+        //student2.getDiary();//--> ilişkiyi diary üzerinden kurduk java kodlari buna ulasamıyoruz o zmn ne yapıcaz DB'den cekebiliriz
+        String diaryName=(String) session.createSQLQuery("select name from t_diary where std_id=1002").uniqueResult();
+        System.out.println(diaryName);
+
+        /*
+        PROBLEM : sorgu yazmadan diaryden studenta ulşabiliyorkey ,studentta diarye ulaşamıyorum
+        ulaşmak istersem?
+
+        //uni_directional(Tek Yönlü) : Diary  -->Student
+        //bi_directional(Çift Yönlü) : Diary <-->Student(veritabanında birşey değişmez) :)
+         */
+
+
+    }
+}
